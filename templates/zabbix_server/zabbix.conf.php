bd287ba65f1412d295426ae4704880ac01a9877a
Matthew Green <matthew@newedgeengineering.com>
2015-02-20 14:38:48 +0000

    formatting

--- templates/zabbix_server/zabbix.conf.php
+++ templates/zabbix_server/zabbix.conf.php
@@ -3,15 +3,15 @@
 // Zabbix GUI configuration file
 global $DB;
 
-$DB['TYPE']     = '{{zabbix_php_db}}';
-$DB['SERVER']   = '{{zabbix_server_db_host}}';
-$DB['PORT']     = '{{zabbix_server_db_port}}';
-$DB['DATABASE'] = '{{zabbix_server_db_name}}';
-$DB['USER']     = '{{zabbix_server_db_user}}';
-$DB['PASSWORD'] = '{{zabbix_server_db_pass}}';
+$DB['TYPE']      = '{{zabbix_php_db}}';
+$DB['SERVER']    = '{{zabbix_server_db_host}}';
+$DB['PORT']      = '{{zabbix_server_db_port}}';
+$DB['DATABASE']  = '{{zabbix_server_db_name}}';
+$DB['USER']      = '{{zabbix_server_db_user}}';
+$DB['PASSWORD']  = '{{zabbix_server_db_pass}}';
 
 // SCHEMA is relevant only for IBM_DB2 database
-$DB['SCHEMA'] = '';
+$DB['SCHEMA']    = '';
 
 $ZBX_SERVER      = '{{zabbix_server_host}}';
 $ZBX_SERVER_PORT = '{{zabbix_server_port}}';
